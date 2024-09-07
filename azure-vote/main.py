3a0f20a8eeb65d37f424cffe45c94a8fe253d968
Quan Nguyen <QuanNCT@outlook.com>
2024-09-07 23:21:23 +0700

    remove port number

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -159,4 +159,4 @@ if __name__ == "__main__":
     # Use the statement below when running locally
     # app.run() 
     # Use the statement below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=False, port=5000) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=False) # remote
